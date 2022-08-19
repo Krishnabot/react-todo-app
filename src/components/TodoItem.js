c6135f87843ff0071ec2e151e1b81c359eb75adf
Krishna <libraryismyparadise@gmail.com>
2022-08-20 00:23:34 +0545

    Update the input tag in the TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,8 +3,11 @@ import React from 'react';
 function TodoItem(props) {
   return (
     <li>
-      <input type="checkbox" />
-      {this.props.todo.title}
+      <input
+        type="checkbox"
+        checked={this.props.todo.completed}
+        onChange={() => console.log("clicked")}
+      />
     </li>
   );
 }

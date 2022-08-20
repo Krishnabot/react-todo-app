47e6bbb3b82a2fee5a7ee3d0c1b77c147dcea9e4
Krishna <libraryismyparadise@gmail.com>
2022-08-20 12:55:35 +0545

    Add  componentWillUnmount logic

--- src/functionBased/TodosList.js
+++ src/functionBased/TodosList.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-class TodosList extends React.Component {
-  render() {
-    <ul>
-      {props.todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          handleChangeProps={this.props.handleChangeProps}
-          deleteTodoProps={this.props.deleteTodoProps}
-        />
-      ))}
-    </ul>;
-  }
-}
-
+const TodosList = (props) => (
+  <ul>
+    {props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        handleChangeProps={props.handleChangeProps}
+        deleteTodoProps={props.deleteTodoProps}
+        setUpdate={props.setUpdate}
+      />
+    ))}
+  </ul>
+);
 export default TodosList;

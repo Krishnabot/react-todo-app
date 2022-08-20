bdbe6399670bb0bfdca0aa0b00d19c594b1c3ab7
Krishna <libraryismyparadise@gmail.com>
2022-08-20 13:08:51 +0545

    Add Nested and Dynamic Routing

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -1,4 +1,28 @@
-import React from "react";
+import React from 'react';
+import { Link, useRouteMatch, Route } from "react-router-dom"
+import SinglePage from "./SinglePage"
+const About = () => {
+  console.log(useRouteMatch())
 
-const About = () => <div>hello from about page</div>;
-export default About;
+  const { url, path } = useRouteMatch()
+Then, update the return statement so you have:
+return (
+  <div>
+    <ul>
+      <li>
+        <Link to={`${url}/about-app`}>About App</Link>
+      </li>
+      <li>
+        <Link to={`${url}/about-author`}>About Author</Link>
+      </li>
+    </ul>
+    <Route path={`${path}/:slug`}>
+      <SinglePage />
+    </Route>
+  </div>
+)
+  return (
+    ...
+  );
+};
+export default About
